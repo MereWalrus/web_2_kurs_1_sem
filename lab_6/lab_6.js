1682c986d20a44857de65802c7fed43fff132212
Eugene Zhavoronok <eugenezhavoronok@mail.ru>
2020-12-20 12:18:53 +0300

    lab 6 add Student component

--- lab_6/lab_6.js
+++ lab_6/lab_6.js
@@ -1,10 +1,28 @@
 'use strict';
 
 function App() {
+  let student = {
+    "name": "Гавриил",
+    "surname": "Лановой",
+    "age": 17,
+    "averageScore": 7
+  };
   return (
-    <div>
-      <p>Бурятия</p>
-    </div>
+    <table>
+      <Student student={student}/>
+    </table>
+  );
+}
+
+function Student(props) {
+  return(
+      <tr rowId={props.student.id}>
+          <td className="name-cell">{props.student.name}</td>
+          <td className="surname-cell">{props.student.surname}</td>
+          <td className="age-cell">{!isNaN(props.student.age) ? props.student.age : ""}</td>
+          <td className="average-score-cell">{!isNaN(props.student.averageScore) ? props.student.averageScore : ""}</td>
+          <td><button onClick={(() => removeStudent(this.parentNode()))}>Удалить</button></td>
+      </tr>
   );
 }
 
